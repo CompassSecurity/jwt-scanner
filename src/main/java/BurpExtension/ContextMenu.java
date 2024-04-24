1527b2bd18e987e9bf9755a112c252dca951b249
Cyrill Bannwart <cyrill.bannwart@compass-security.com>
2024-04-24 12:46:36 +0000

    Bugfix ContextMenu

--- src/main/java/BurpExtension/ContextMenu.java
+++ src/main/java/BurpExtension/ContextMenu.java
@@ -11,11 +11,13 @@ package BurpExtension;
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.core.Range;
 import burp.api.montoya.core.ToolType;
+import burp.api.montoya.http.Http;
 import burp.api.montoya.http.message.HttpRequestResponse;
 import burp.api.montoya.scanner.Scanner;
 import burp.api.montoya.ui.Selection;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
 import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
+import burp.api.montoya.ui.contextmenu.MessageEditorHttpRequestResponse;
 import burp.api.montoya.ui.editor.HttpRequestEditor;
 
 import javax.swing.*;
@@ -44,30 +46,49 @@ public class ContextMenu implements ContextMenuItemsProvider
     {
         if (event.isFromTool(ToolType.PROXY, ToolType.REPEATER, ToolType.TARGET, ToolType.LOGGER, ToolType.INTRUDER))
         {
+            MessageEditorHttpRequestResponse editorHttpRequestResponse = null;
+            HttpRequestResponse requestResponse;
+
+            boolean editorIsPresent = event.messageEditorRequestResponse().isPresent();
+
             List<Component> menuItemList = new ArrayList<>();
 
-            JMenuItem retrieveRequestItem = new JMenuItem("Autodetect JWT");
+            if (editorIsPresent) {
+                editorHttpRequestResponse = event.messageEditorRequestResponse().get();
+                requestResponse =  editorHttpRequestResponse.requestResponse();
+            } else {
+                List<HttpRequestResponse> selectedRequests = event.selectedRequestResponses();
 
-            HttpRequestResponse requestResponse = event.messageEditorRequestResponse().isPresent() ? event.messageEditorRequestResponse().get().requestResponse() : event.selectedRequestResponses().get(0);
+                // only 1 request is support at this time
+                if (selectedRequests.size() == 1) {
+                    requestResponse = selectedRequests.get(0);
+                } else {
+                    return null;
+                }
+            }
+
+            // Autodetect JWT
+            JMenuItem retrieveRequestItem = new JMenuItem("Autodetect JWT");
 
             JWTScanCheck scan = new JWTScanCheck(api);
             JwtInsertionPoint insertionPoint = new JwtInsertionPoint(api,requestResponse.request());
             retrieveRequestItem.addActionListener(l -> SwingUtilities.invokeLater(() ->
-                this.executor.execute(() -> scan.activeAudit(requestResponse,insertionPoint)))
+                    this.executor.execute(() -> scan.activeAudit(requestResponse,insertionPoint)))
             );
             menuItemList.add(retrieveRequestItem);
 
-            if (event.messageEditorRequestResponse().get().selectionOffsets().isPresent()) {
+            // Selected JWT
+            if (editorIsPresent && editorHttpRequestResponse.selectionOffsets().isPresent()) {
                 JMenuItem retrieveSelectedRequestItem = new JMenuItem("Selected JWT");
                 int startindex = event.messageEditorRequestResponse().get().selectionOffsets().get().startIndexInclusive();
                 int endindex = event.messageEditorRequestResponse().get().selectionOffsets().get().endIndexExclusive();
                 JWTScanCheck scanSelected = new JWTScanCheck(api);
                 JwtInsertionPoint insertionPointSelected = new JwtInsertionPoint(api,requestResponse.request(),startindex,endindex);
                 retrieveSelectedRequestItem.addActionListener(l -> SwingUtilities.invokeLater(() ->
-                        this.executor.execute(() -> scanSelected.activeAudit(requestResponse,insertionPointSelected)))
+                    this.executor.execute(() -> scanSelected.activeAudit(requestResponse,insertionPointSelected)))
                 );
-                menuItemList.add(retrieveSelectedRequestItem);
 
+                menuItemList.add(retrieveSelectedRequestItem);
             }
 
             return menuItemList;
