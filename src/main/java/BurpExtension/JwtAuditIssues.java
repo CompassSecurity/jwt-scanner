3d05799868170309550aa62aa74297eb25f05455
Cyrill Bannwart <cyrill.bannwart@compass-security.com>
2024-05-13 17:26:23 +0000

    Rename variable

--- src/main/java/BurpExtension/JwtAuditIssues.java
+++ src/main/java/BurpExtension/JwtAuditIssues.java
@@ -9,7 +9,7 @@ import static burp.api.montoya.scanner.audit.issues.AuditIssue.auditIssue;
 
 public abstract class JwtAuditIssues {
 
-    public static AuditIssue expired(HttpRequestResponse baseRequestResponse, HttpRequestResponse checkRequestResponse){
+    public static AuditIssue expired(HttpRequestResponse baseRequestResponse, HttpRequestResponse markedRequestResponse){
         return auditIssue("Expired JWT accepted",
                 "The server accepts JWTs that are expired \n " +
                         "An attacker can use an expired JWT.",
@@ -22,10 +22,10 @@ public abstract class JwtAuditIssues {
                 null,
                 null,
                 AuditIssueSeverity.HIGH,
-                checkRequestResponse);
+                markedRequestResponse);
     }
 
-    public static AuditIssue withoutSignature(HttpRequestResponse baseRequestResponse, HttpRequestResponse checkRequestResponse){
+    public static AuditIssue withoutSignature(HttpRequestResponse baseRequestResponse, HttpRequestResponse markedRequestResponse){
         return auditIssue("JWT Signature not required",
                 "The server accepts JWTs that are not signed \n " +
                         "An attacker can forge a JWT and take over any account and role in the application. " +
@@ -37,10 +37,10 @@ public abstract class JwtAuditIssues {
                 null,
                 null,
                 AuditIssueSeverity.HIGH,
-                checkRequestResponse);
+                markedRequestResponse);
     }
 
-    public static AuditIssue getAlgNone(HttpRequestResponse baseRequestResponse, HttpRequestResponse checkRequestResponse){
+    public static AuditIssue getAlgNone(HttpRequestResponse baseRequestResponse, HttpRequestResponse markedRequestResponse){
          return auditIssue("Algorithm none JWT attack",
                 "The server accepts JWTs created with the \"none\" algorithm. \n " +
                         "The JWT ànoneà algorithm is a waz of creating a JWT without adding a signature. " +
@@ -55,10 +55,10 @@ public abstract class JwtAuditIssues {
                 null,
                 null,
                 AuditIssueSeverity.HIGH,
-                checkRequestResponse);
+                markedRequestResponse);
     }
 
-    public static AuditIssue invalidSignature(HttpRequestResponse baseRequestResponse, HttpRequestResponse checkRequestResponse){
+    public static AuditIssue invalidSignature(HttpRequestResponse baseRequestResponse, HttpRequestResponse markedRequestResponse){
         return auditIssue("Invalid JWT Signature",
                 "The signature of the JSON Web Tokens (JWT) is not checked by the server.\n" +
                         "An attacker can forge a JWT and take over any account and role in the application. " +
@@ -71,10 +71,10 @@ public abstract class JwtAuditIssues {
                 null,
                 null,
                 AuditIssueSeverity.HIGH,
-                checkRequestResponse);
+                markedRequestResponse);
     }
 
-    public static AuditIssue emptyPassword(HttpRequestResponse baseRequestResponse, HttpRequestResponse checkRequestResponse){
+    public static AuditIssue emptyPassword(HttpRequestResponse baseRequestResponse, HttpRequestResponse markedRequestResponse){
         return auditIssue("JWT signed with empty password",
                 "The signature of the JSON Web Tokens (JWT) is created with an empty password.\n" +
                         "An attacker can forge a JWT with an empty password and take over any account and role in the application. " +
@@ -88,9 +88,9 @@ public abstract class JwtAuditIssues {
                 null,
                 null,
                 AuditIssueSeverity.HIGH,
-                checkRequestResponse);
+                markedRequestResponse);
     }
-    public static AuditIssue invalidEcdsa(HttpRequestResponse baseRequestResponse, HttpRequestResponse checkRequestResponse){
+    public static AuditIssue invalidEcdsa(HttpRequestResponse baseRequestResponse, HttpRequestResponse markedRequestResponse){
         return auditIssue("JWT signed invalid ECDSA parameters",
                 "CVE-2022-21449 Vulnerability in the Oracle Java SE, Oracle GraalVM Enterprise Edition product of Oracle Java SE.\n" +
                         "Easily exploitable vulnerability allows unauthenticated attacker with network access via multiple protocols to compromise Oracle Java SE," +
@@ -103,9 +103,9 @@ public abstract class JwtAuditIssues {
                 null,
                 null,
                 AuditIssueSeverity.HIGH,
-                checkRequestResponse);
+                markedRequestResponse);
     }
-    public static AuditIssue jwksInjection(HttpRequestResponse baseRequestResponse, HttpRequestResponse checkRequestResponse){
+    public static AuditIssue jwksInjection(HttpRequestResponse baseRequestResponse, HttpRequestResponse markedRequestResponse){
         return auditIssue("JWT JWKs Injection",
                 "It is possible to include the used public key in the JWK value of the header. The Application takes the included public key to validate the signature",
                 "The JWK provided in the header should not be used to validate the signature.",
@@ -115,7 +115,7 @@ public abstract class JwtAuditIssues {
                 null,
                 null,
                 AuditIssueSeverity.HIGH,
-                checkRequestResponse);
+                markedRequestResponse);
     }
 
 }
